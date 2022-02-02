11e049b573ac8bca1606319fb395f45ed12a6ad2
Thomas Scheiblauer <tom@sharkbay.at>
2022-02-02 13:00:29 +0100

    add newline to end of file

--- addons.tf
+++ addons.tf
@@ -5,4 +5,3 @@ data "http" "ccm_manifest" {
 data "http" "hcloud_csi_driver_manifest" {
   url = "https://raw.githubusercontent.com/hetznercloud/csi-driver/${var.hcloud_csi_driver_version}/deploy/kubernetes/hcloud-csi.yml"
 }
-
