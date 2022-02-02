f533f9663297c231f6cc4cadaed666b7d88297a7
Thomas Scheiblauer <tom@sharkbay.at>
2022-02-02 21:21:15 +0100

    include ccm yaml as template again but with parameterized image tag

--- addons.tf
+++ addons.tf
@@ -1,5 +1,8 @@
-data "http" "ccm_manifest" {
-  url = "https://raw.githubusercontent.com/hetznercloud/hcloud-cloud-controller-manager/${var.hcloud_ccm_version}/deploy/ccm-networks.yaml"
+data "template_file" "ccm_manifest" {
+  template = file("${path.module}/manifests/ccm-networks.yaml")
+  vars = {
+    hcloud_ccm_version = var.hcloud_ccm_version
+  }
 }
 
 data "http" "hcloud_csi_driver_manifest" {
